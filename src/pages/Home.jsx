62b77125a5d9700d35acc4a194d32e478e0a50bf
Yuvraj Singh <singhyuvr.aj1211.gs@gmail.com>
2025-06-12 09:31:47 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+// Home.jsx
+import React from 'react';
+const Home = () => (
+  <div>
+    <h1>Welcome to ProjectSKY</h1>
+    <p>This is the home page.</p>
+  </div>
+);
+export default Home;
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,9 @@
+// Pricing.jsx
+import React from 'react';
+const Pricing = () => (
+  <div>
+    <h1>Pricing Plans</h1>
+    <p>Here are the subscription options.</p>
+  </div>
+);
+export default Pricing;
