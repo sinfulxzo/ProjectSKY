9cec7b660b403b454a5b8166f69b345254c3824a
sinfulxzo <singhyuvr.aj1211.ys@gmail.com>
2025-06-17 11:14:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    allowedHosts: ['projectsky.onrender.com'],
     host: "::",
     port: 8080,
   },
